1e46bb4c1ea6863cee277ee5b9b88d3f24f94ddf
jeronimo10000 <jeronimo000000@gmail.com>
2016-03-16 19:16:52 -0300

    teste 1

--- src/decisao/Exercicio5.java
+++ src/decisao/Exercicio5.java
@@ -14,7 +14,7 @@ public class Exercicio5 {
 		 * A mensagem "Reprovado", se a média for menor do que sete;
 		 * A mensagem "Aprovado com Distinção", se a média for igual a dez.
 		 */
-		//jkjlkjljljl
+		
 		
 		ler = new Scanner(System.in);
 		
